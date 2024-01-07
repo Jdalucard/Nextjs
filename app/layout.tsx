6338965d641b01b9c9ff072982ea301561746e4d
Jose daniel martinez <danieluz3188@gmail.com>
2024-01-07 08:25:29 -0500

    practice

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import './ui/global.css';
+import { poppins } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className={`${poppins.className} antialiased`}>{children}</div>
+        <footer>footer para todos</footer>
+      </body>
     </html>
   );
 }
